d6d0995757742ac4d330b197610f63f038c264e2
Shukurjon Boqiyev <shukurjonboqiyev0@gmail.com>
2024-07-14 11:54:20 +0500

    click app

---
+++ src/main/java/com/webapp/clicup/ClicUpApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.clicup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClicUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/clicup/ClicUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.clicup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
